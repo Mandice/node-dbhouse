49f96757a0eff71ed157ae375d6b7883bd6b0976
Fred Chien <fred@mandice.com>
2012-08-25 14:00:29 +0800

    support selecting specific fields

--- tests/query1.js
+++ tests/query1.js
@@ -8,6 +8,22 @@ dbHouse.connect('mongodb', { host: 'localhost', port: 27017 }, function() {
 	var db = new DBHouse.Database(dbHouse);
 	db.open('dbhouse')
 		.collection('users')
+		.select({
+			email: 0
+		})
+		.where({
+			'$or': [ { name: 'Fred Chien'}, { name: 'Stacy Li' } ]
+		})
+		.limit(1)
+		.query(function(err, data) {
+			if (err)
+				throw err;
+
+			console.log(data);
+		});
+
+	db.open('dbhouse')
+		.collection('users')
 		.where({
 			'$or': [ { name: 'Fred Chien'}, { name: 'Stacy Li' } ]
 		})
